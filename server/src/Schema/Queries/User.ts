786b6b94e0d882863d0568c9e4b3911dcf75a7f7
yariknashivan <yarik.nashivan@gmail.com>
2022-06-30 23:43:22 +0300

    queries and mutations

---
+++ server/src/Schema/Queries/User.ts
@@ -0,0 +1,10 @@
+import { UserType } from '../TypeDefs/User';
+import { GraphQLList } from "graphql";
+import { Users } from "../../Entities/Users";
+
+export const GET_ALL_USERS = {
+    type: new GraphQLList(UserType),
+    resolve() {
+        return Users.find();
+    },
+};
